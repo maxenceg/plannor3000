2480c283bdc7529e20f3069307ed5bd84fed871a
Maxence Gilliot <maxencegilliot@gmail.com>
2019-01-14 13:53:15 +0100

    Change name to plannor 3000

--- client/src/components/Root/Root.js
+++ client/src/components/Root/Root.js
@@ -31,7 +31,7 @@ export default class Root extends React.Component {
           <div className="header">
             <img src={logo} className="logo" alt="logo" />
             <Typography variant="h2" color="primary">
-              Welcome to Francis
+              Welcome to Plannor 3000
             </Typography>
           </div>
           {children}
