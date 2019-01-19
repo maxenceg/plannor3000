706758361c64dcc26f4c364c0ff98c5168512309
Maxence Gilliot <maxencegilliot@gmail.com>
2019-01-19 20:40:35 +0100

    Use component in dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import Menu from '../Menu';
 import styles from './Dashboard.style';
+import ProdPlan from '../ProdPlan';
 
 export default class Dashboard extends React.Component {
   render() {
     return (
       <div style={styles.dashboard}>
         <Menu style={styles.menu} />
+        <ProdPlan style={styles.prodPlan} />
       </div>
     );
   }
--- client/src/components/Dashboard/Dashboard.style.js
+++ client/src/components/Dashboard/Dashboard.style.js
@@ -5,6 +5,10 @@ const styles = {
   menu: {
     padding: 15,
     fontFamily: 'Roboto',
+    width: 200,
+  },
+  prodPlan: {
+    width: '100%',
   },
 };
 
