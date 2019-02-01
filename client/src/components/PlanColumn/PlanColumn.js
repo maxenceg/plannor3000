1552dd3f6757fdce8eb11dd6b72a3c88d96d40f6
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-01 20:41:39 +0100

    Use fixture data to display cards

--- client/src/components/PlanColumn/PlanColumn.js
+++ client/src/components/PlanColumn/PlanColumn.js
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import styles from './PlanColumn.style.js';
-import ColumnHeader from '../ColumnHeader/index.js';
 import PlanCard from '../PlanCard';
 
 export default class PlanColumn extends React.Component {
@@ -9,12 +8,15 @@ export default class PlanColumn extends React.Component {
     return (
       <div style={styles.container}>
         <div style={{ ...this.props.style, ...styles.grid }}>
-          <PlanCard
-            dayStartTime={this.props.dayStartTime}
-            dayEndTime={this.props.dayEndTime}
-            startTime={{ hour: 9, minute: 30 }}
-            endTime={{ hour: 15, minute: 0 }}
-          />
+          {this.props.cards.map(card => (
+            <PlanCard
+              key={card.id}
+              dayStartTime={this.props.dayStartTime}
+              dayEndTime={this.props.dayEndTime}
+              startTime={card.startTime}
+              endTime={card.endTime}
+            />
+          ))}
         </div>
       </div>
     );
