2166a26095e95778052aa25b0ffbe89a2ac03043
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-01 20:42:29 +0100

    Handle connection with Trello

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,8 +1,33 @@
 // @flow
 import React from 'react';
+import BasicButton from '../BasicButton';
 
 export default class Menu extends React.Component {
   render() {
-    return <div style={this.props.style}>Left Menu</div>;
+    const connectToTrello = () => {
+      var authenticationSuccess = function() {
+        console.log('Successful authentication');
+      };
+
+      var authenticationFailure = function() {
+        console.log('Failed authentication');
+      };
+      window.Trello.authorize({
+        type: 'popup',
+        name: 'Plannor 3000',
+        scope: {
+          read: 'true',
+          write: 'true',
+        },
+        expiration: 'never',
+        success: authenticationSuccess,
+        error: authenticationFailure,
+      });
+    };
+    return (
+      <div style={this.props.style}>
+        <BasicButton label="Se connecter avec Trello" onClickAction={connectToTrello} />
+      </div>
+    );
   }
 }
