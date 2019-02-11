2f53f63d9cd07cc8dfd71d708d32b0bb4c9426ef
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-11 18:19:00 +0100

    Display card as unstyled list

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -19,11 +19,24 @@ export default class Menu extends React.Component {
       this.props.fetchTrelloUser();
       this.props.fetchTrelloUserBoards();
     };
-    const basicSelectOptions = this.props.trelloUserBoards
+    const boardSelectOptions = this.props.trelloUserBoards
       ? this.props.trelloUserBoards.map(function(board) {
           return { value: board.id, label: board.name };
         })
       : null;
+    const dailyGoalsSelectOptions = this.props.trelloUserColumns
+      ? this.props.trelloUserColumns.map(function(column) {
+          return { value: column.id, label: column.name };
+        })
+      : null;
+    const handleBoardChange = event => {
+      this.props.addTrelloUserSelectedBoard(event.target.value);
+      this.props.fetchTrelloUserColumns(event.target.value);
+    };
+    const handleDailyGoalsChange = event => {
+      this.props.addTrelloUserDailyGoalsColumn(event.target.value);
+      this.props.fetchDailyGoalsCards(event.target.value);
+    };
     return (
       <div style={this.props.style}>
         {!this.props.trelloUserFullName && (
@@ -33,11 +46,28 @@ export default class Menu extends React.Component {
           <BasicSelect
             propStyle={styles.boardSelect}
             value={this.props.trelloUserSelectedBoard}
-            handleChange={event => this.props.addTrelloUserSelectedBoard(event.target.value)}
-            label="Séléctionnez un board"
-            options={basicSelectOptions}
+            handleChange={handleBoardChange}
+            label="Sélectionnez un board"
+            options={boardSelectOptions}
           />
         )}
+        {this.props.trelloUserSelectedBoard !== '' && (
+          <BasicSelect
+            propStyle={styles.boardSelect}
+            value={this.props.trelloUserDailyGoalsColumn}
+            handleChange={handleDailyGoalsChange}
+            label="Sélectionnez la colonne Daily Goals"
+            options={dailyGoalsSelectOptions}
+          />
+        )}
+        {this.props.trelloUserDailyGoalsCards.length > 0 &&
+          this.props.trelloUserDailyGoalsCards.map(card => (
+            <span key={card.id}>
+              {card.shortId}
+              {card.name}
+              <br />
+            </span>
+          ))}
       </div>
     );
   }
