ac2c0f8e78f705e5aeadecfea4043b4ce3edda7c
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-11 18:18:28 +0100

    Pass functions to the menu

--- client/src/components/Menu/Menu.wrap.js
+++ client/src/components/Menu/Menu.wrap.js
@@ -2,7 +2,10 @@ import Menu from './Menu';
 import {
   fetchTrelloUserBoards,
   fetchTrelloUser,
+  fetchTrelloUserColumns,
+  fetchDailyGoalsCards,
   addTrelloUserSelectedBoard,
+  addTrelloUserDailyGoalsColumn,
 } from 'redux/TrelloUser/actions';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -10,18 +13,27 @@ import {
   getTrelloUserFullName,
   getTrelloUserBoards,
   getTrelloUserSelectedBoard,
+  getTrelloUserColumns,
+  getTrelloUserDailyGoalsColumn,
+  getTrelloUserDailyGoalsCards,
 } from 'redux/TrelloUser';
 
 const mapStateToProps = state => ({
   trelloUserFullName: getTrelloUserFullName(state),
   trelloUserBoards: getTrelloUserBoards(state),
   trelloUserSelectedBoard: getTrelloUserSelectedBoard(state),
+  trelloUserColumns: getTrelloUserColumns(state),
+  trelloUserDailyGoalsColumn: getTrelloUserDailyGoalsColumn(state),
+  trelloUserDailyGoalsCards: getTrelloUserDailyGoalsCards(state),
 });
 
 const mapDispatchToProps = {
   fetchTrelloUser,
   fetchTrelloUserBoards,
+  fetchTrelloUserColumns,
+  fetchDailyGoalsCards,
   addTrelloUserSelectedBoard,
+  addTrelloUserDailyGoalsColumn,
 };
 
 export default connect(
