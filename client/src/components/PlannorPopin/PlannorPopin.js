d2a8981ddc1583fd6e5d8e227dc3024c09eb5d43
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-16 20:36:41 +0100

    Create basic popin component

---
+++ client/src/components/PlannorPopin/PlannorPopin.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import styles from './PlannorPopin.style';
+
+export default class PlanCard extends React.Component {
+  render() {
+    return <div style={styles.popInContainer}>Hello world</div>;
+  }
+}
---
+++ client/src/components/PlannorPopin/PlannorPopin.style.js
@@ -0,0 +1,17 @@
+const styles = {
+  popInContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    padding: 30,
+    background: 'white',
+    MozBoxShadow: '2px 2px 10px #cccccc',
+    WebkitBoxShadow: '2px 2px 10px #cccccc',
+    boxShadow: '2px 2px 10px #cccccc',
+    zIndex: 1,
+    borderRadius: 10,
+  },
+};
+
+export default styles;
