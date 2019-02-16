f6d0b73fa2b60f850eae2c7c55ef0a776395c696
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-16 19:17:10 +0100

    Create new component buttonsPanel

---
+++ client/src/components/ButtonsPanel/ButtonsPanel.js
@@ -0,0 +1,26 @@
+// @flow
+import React from 'react';
+import BasicButton from '../BasicButton';
+import styles from './ButtonsPanel.style';
+
+export default class ButtonsPanel extends React.Component {
+  render() {
+    return (
+      <div style={this.props.style}>
+        <BasicButton
+          style={{ ...styles.panelButton, ...styles.settingsButton }}
+          label={<i className="material-icons">settings</i>}
+        />
+        <BasicButton
+          style={{ ...styles.panelButton, ...styles.editTeamButton }}
+          label={<i className="material-icons">people</i>}
+        />
+        <BasicButton
+          style={{ ...styles.panelButton, ...styles.refreshButton }}
+          label={<i className="material-icons">refresh</i>}
+          onClickAction={this.props.refreshAction}
+        />
+      </div>
+    );
+  }
+}
---
+++ client/src/components/ButtonsPanel/ButtonsPanel.style.js
@@ -0,0 +1,18 @@
+const styles = {
+  panelButton: {
+    minWidth: 'auto',
+    minHeight: 'auto',
+    width: '32px',
+    height: '32px',
+    padding: 0,
+  },
+  settingsButton: {
+    marginRight: 5,
+  },
+  editTeamButton: {
+    marginRight: 5,
+  },
+  refreshButton: {},
+};
+
+export default styles;
