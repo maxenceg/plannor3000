fea784b6e40ab30a1f116ce5fb0fd5552d89bf5e
Victor <v.gobel@hotmail.fr>
2019-02-20 14:01:19 +0100

    add new entity and migration

--- api/src/Entity/Board.php
+++ api/src/Entity/Board.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * A board.
  *
+ * @ORM\Table(name="plannor_boards")
  * @ORM\Entity
  */
 class Board
---
+++ api/src/Migrations/Version20190220125050.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Creation of an Entity Board
+ */
+final class Version20190220125050 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE plannor_boards (id INT NOT NULL, trello_board_id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE plannor_boards');
+    }
+}
