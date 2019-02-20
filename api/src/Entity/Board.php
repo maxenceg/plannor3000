3839633b70a39bc3870e103dceb549c617e7a292
Maxence Gilliot <maxencegilliot@gmail.com>
2019-02-20 13:44:01 +0100

    Create entity board

---
+++ api/src/Entity/Board.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A board.
+ *
+ * @ORM\Entity
+ */
+class Board
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $trelloBoardId;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTrelloBoardId() {
+        return $this->trelloBoardId;
+    }
+
+    public function setTrelloBoardId($trelloBoardId) {
+        $this->trelloBoardId = $trelloBoardId;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+}
