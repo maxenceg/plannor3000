890aa45a831c7f764f30ea4f264d8965a7aace0a
Maxence Gilliot <maxencegilliot@gmail.com>
2019-03-16 23:17:39 +0100

    Create helpers to manipulate time

--- client/src/helpers/TimeHelpers/timeHelpers.js
+++ client/src/helpers/TimeHelpers/timeHelpers.js
@@ -5,3 +5,16 @@ export function timeInMinutes(time) {
 export function durationInMinutes(startTime, endTime) {
   return timeInMinutes(endTime) - timeInMinutes(startTime);
 }
+
+export function splitTimeFromString(timeString) {
+  const startTime = timeString.split(':');
+  return { hour: parseInt(startTime[0]), minute: parseInt(startTime[1]) };
+}
+
+export function stringTimeFromObject(timeObject) {
+  const hours =
+    timeObject.hour >= 10 ? timeObject.hour.toString() : '0' + timeObject.hour.toString();
+  const minutes =
+    timeObject.minute >= 10 ? timeObject.minute.toString() : '0' + timeObject.minute.toString();
+  return hours + ':' + minutes;
+}
