cec383a46011bfd5a314e8f2efdfe250e89cb8ad
Maxence Gilliot <maxencegilliot@gmail.com>
2019-04-11 13:39:13 +0200

    Remove useless imports

--- client/src/components/ProdPlan/ProdPlan.wrap.js
+++ client/src/components/ProdPlan/ProdPlan.wrap.js
@@ -1,16 +1,10 @@
 import ProdPlan from './ProdPlan';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import {
-  getTrelloUserDevTeam,
-  getTrelloUserDGCardsRelatedToMember,
-  getTrelloUserDailyGoalsColumn,
-} from 'redux/TrelloUser';
+import { getTrelloUserDevTeam, getTrelloUserDailyGoalsColumn } from 'redux/TrelloUser';
 
 const mapStateToProps = state => ({
   devTeamMembers: getTrelloUserDevTeam(state),
-  getDailyGoalsCardsRelatedToMember: memberId =>
-    getTrelloUserDGCardsRelatedToMember(state, memberId),
   dailyGoalsColumn: getTrelloUserDailyGoalsColumn(state),
 });
 
