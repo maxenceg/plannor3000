ba89efa892bb05cc38ffe94159a129ad3f7fb472
Victor <v.gobel@hotmail.fr>
2019-04-27 17:25:40 +0200

    First try of sass file

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { IntlProvider, addLocaleData } from 'react-intl';
 import fr from 'react-intl/locale-data/fr';
 import en from 'react-intl/locale-data/en';
 import { ReactComponent as Logo } from '../../assets/plannor3000-logo.svg';
+import './Header.scss'
 
 import frMessages from 'translations/fr.json';
 import enMessages from 'translations/en.json';
@@ -21,8 +22,8 @@ export default class Header extends React.Component {
   render() {
     return (
       <IntlProvider locale="fr" messages={locales.fr}>
-        <div style={styles.header}>
-          <Logo style={styles.logo} />
+        <div className="header">
+          <Logo className="header__logo" />
         </div>
       </IntlProvider>
     );
