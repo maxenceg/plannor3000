1499c8e63d8fd03046277726bade1f5cba8549e8
Maxence Gilliot <maxencegilliot@gmail.com>
2019-07-10 13:10:41 +0200

    [64] Use selector to get data from board

--- client/src/components/PlanColumn/PlanColumn.js
+++ client/src/components/PlanColumn/PlanColumn.js
@@ -16,6 +16,30 @@ export default class PlanColumn extends React.Component {
       <div style={styles.container}>
         <div style={{ ...this.props.style, ...styles.grid }}>
           {map(cards, card => {
+            let startTime;
+            if (!card.checklists) {
+              startTime = { hour: 10, minute: 0 };
+            } else {
+              const timeData = card.checklists
+                .find(checklist => checklist.name === 'Plannor 3000')
+                .checkItems.find(item => item.name.includes('Start'))
+                .name.match(/\d+:\d+/)[0];
+              const splitTimeData = timeData.split(':');
+              startTime = { hour: parseInt(splitTimeData[0]), minute: parseInt(splitTimeData[1]) };
+            }
+
+            let endTime;
+            if (!card.checklists) {
+              endTime = { hour: 10, minute: 0 };
+            } else {
+              const timeData = card.checklists
+                .find(checklist => checklist.name === 'Plannor 3000')
+                .checkItems.find(item => item.name.includes('End'))
+                .name.match(/\d+:\d+/)[0];
+              const splitTimeData = timeData.split(':');
+              endTime = { hour: parseInt(splitTimeData[0]), minute: parseInt(splitTimeData[1]) };
+            }
+
             return (
               <PlanCard
                 key={card.id}
@@ -23,9 +47,9 @@ export default class PlanColumn extends React.Component {
                 devName={this.props.member.fullName}
                 dayStartTime={this.props.dayStartTime}
                 dayEndTime={this.props.dayEndTime}
-                startTime={card.startTime}
-                endTime={card.endTime}
-                content={card.name}
+                startTime={startTime}
+                endTime={endTime}
+                content={cards.name}
                 icons={[{ name: 'edit', action: editDevSelection }]}
               />
             );
